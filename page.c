539416244ec2713b5e250e6af58d58f819b1a929
lokireturns <chachipereira0@gmail.com>
2026-01-17 12:47:47 +0000

    Where to read & write

--- page.c
+++ page.c
@@ -13,3 +13,16 @@ void deserialise_row(void *source, Row *destination) {
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+// Where to read/write in memory for a row
+void* row_slot(Table *table, uint32_t row_num){
+    uint32_t page_num = row_num / ROWS_PER_PAGE; // Remember: Integer division in C
+    void* page = table->pages[page_num];
+    if(page == NULL) { // If valid memory addr
+        void *allocated_memory = malloc(PAGE_SIZE);
+        table->pages[page_num] = allocated_memory;
+        page = allocated_memory;
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE; // Offset within the page
+    return page + byte_offset;
+}
